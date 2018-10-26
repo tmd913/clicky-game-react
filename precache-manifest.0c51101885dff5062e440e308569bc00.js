8047d48224275d65fe0c9e348f7d2902d2eabeeb
Tom Dennis <tdennis913@gmail.com>
2018-10-25 22:55:51 -0400

    Updates

---
+++ precache-manifest.0c51101885dff5062e440e308569bc00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "26f59f487fa85d0637cc",
+    "url": "/clicky-game-react/static/js/main.26f59f48.chunk.js"
+  },
+  {
+    "revision": "f843a591dc3acd9073f0",
+    "url": "/clicky-game-react/static/js/1.f843a591.chunk.js"
+  },
+  {
+    "revision": "26f59f487fa85d0637cc",
+    "url": "/clicky-game-react/static/css/main.3fda7544.chunk.css"
+  },
+  {
+    "revision": "b599953f2ac01baf772527828c0db9b5",
+    "url": "/clicky-game-react/index.html"
+  }
+];
