00e057c239480fa7ed0fc2b2147809fe95b16c79
Tom Dennis <tdennis913@gmail.com>
2019-01-03 19:00:57 -0500

    Updates

---
+++ precache-manifest.a1f760c69619e380653d168156d8fb38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "3be24c92b87b5bc85500",
+    "url": "/clicky-game-react/static/js/main.3be24c92.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/clicky-game-react/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "3be24c92b87b5bc85500",
+    "url": "/clicky-game-react/static/css/main.3667cd9c.chunk.css"
+  },
+  {
+    "revision": "ddf4e1daadd7da2ddeed1b174db2d987",
+    "url": "/clicky-game-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/clicky-game-react/precache-manifest.d1616c0a032e8148a293835d0baff6e9.js"
+  "/clicky-game-react/precache-manifest.a1f760c69619e380653d168156d8fb38.js"
 );
 
 workbox.clientsClaim();
